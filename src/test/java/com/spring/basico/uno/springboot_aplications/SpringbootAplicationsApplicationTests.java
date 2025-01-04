a86aff7a6bd332d86318bbbabb64615acdd819f4
Fabián Paucar <faibol2000@gmail.com>
2025-01-03 22:04:42 -0500

    primer programa básico

---
+++ src/test/java/com/spring/basico/uno/springboot_aplications/SpringbootAplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.basico.uno.springboot_aplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
