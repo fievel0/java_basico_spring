166014fa2448d21188693fca1d3425402c85b7ac
Fabián Paucar <faibol2000@gmail.com>
2025-01-07 23:16:11 -0500

    Inyectando dependencias

--- src/main/java/com/spring/basico/uno/springboot_aplications/controllers/EjemploController.java
+++ src/main/java/com/spring/basico/uno/springboot_aplications/controllers/EjemploController.java
@@ -1,6 +1,7 @@
 package com.spring.basico.uno.springboot_aplications.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,9 +9,10 @@ public class EjemploController {
 
     @GetMapping("/detalles_info")
 
-
-    public String info(){
-
+    public String info(Model model){
+        model.addAttribute("Titulo", "Servidor en linea");
+        model.addAttribute("Servidor", "INT-SOLUTIONS");
+        model.addAttribute("Ip", "192.168.1.1");
         return "detalles_info";
     }
 }
