08af6875aa1bbab4c8182f4fa15f58631abd90a8
Fabián Paucar <faibol2000@gmail.com>
2025-01-07 22:33:12 -0500

    primera aplicacion en spring boot

--- src/main/java/com/spring/basico/uno/springboot_aplications/controllers/EjemploController.java
+++ src/main/java/com/spring/basico/uno/springboot_aplications/controllers/EjemploController.java
@@ -3,12 +3,12 @@ package com.spring.basico.uno.springboot_aplications.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class EjemploController {
 
     @GetMapping("/detalles_info")
-  
+
+
     public String info(){
 
         return "detalles_info";
